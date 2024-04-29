6d037d7e3279efa34cd5191ab9c7185f50ee2ecd
torbenish <ishiitorben@gmail.com>
2024-04-29 17:09:40 -0300

    Add functions 3-7

--- functions/3/main.go
+++ functions/3/main.go
@@ -2,14 +2,19 @@ package main
 
 const a = "Hello, World!"
 
+type ID int
+
 var (
 	b bool    = true
 	c int     = 10
 	d string  = "Teste"
 	e float64 = 1.2
+	f ID      = 1
 )
 
 func main() {
-	a := "X"  // string
-	println(a)
+	println(f)
+}
+
+func x() {
 }
