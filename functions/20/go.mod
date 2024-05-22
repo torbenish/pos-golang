e717b46993aed8b3db0a18712207d83d1a961101
torbenish <ishiitorben@gmail.com>
2024-05-22 17:16:09 -0300

    Add packages and modules

---
+++ functions/20/go.mod
@@ -0,0 +1,3 @@
+module pos-golang
+
+go 1.22.3
---
+++ functions/20/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"pos-golang/matematics"
+)
+
+func main() {
+	s := matematics.Sum(10, 20)
+	fmt.Println("Result: ", s)
+}
