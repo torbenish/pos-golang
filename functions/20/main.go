6873b2dfcd73334acabb677b4aec17f69499c72e
torbenish <ishiitorben@gmail.com>
2024-05-23 17:05:56 -0300

    More packages and modules

--- functions/20/main.go
+++ functions/20/main.go
@@ -7,5 +7,9 @@ import (
 
 func main() {
 	s := matematics.Sum(10, 20)
+	car := matematics.Car{Brand: "Fiat"}
+	fmt.Println(car.Brand)
+	fmt.Println(car.Walk())
 	fmt.Println("Result: ", s)
+	fmt.Println(matematics.A)
 }
