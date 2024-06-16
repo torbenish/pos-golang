211d8b94d30828276e288dcdd2dc0ee667c3dd2f
torbenish <ishiitorben@gmail.com>
2024-06-16 16:20:01 -0300

    Add GORM/Firsts-consultations

--- 04-Database/2-GORM/main.go
+++ 04-Database/2-GORM/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -10,6 +12,7 @@ type Product struct {
 	Name  string
 	Price float64
 }
+
 func main() {
 	dsn := "root:root@tcp(localhost:3306)/goexpert"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -25,11 +28,24 @@ func main() {
 	// })
 
 	// create batch
-	products := []Product{
-		{Name: "Laptop", Price: 1000.00},
-		{Name: "Mouse", Price: 50.00},
-		{Name: "Keyboard", Price: 75.00},
+	// products := []Product{
+	// 	{Name: "Laptop", Price: 1000.00},
+	// 	{Name: "Mouse", Price: 50.00},
+	// 	{Name: "Keyboard", Price: 75.00},
+	// }
+	// db.Create(&products)
+
+	// select one
+	var product Product
+	// db.First(&product, 3)
+	// fmt.Println(product)
+	db.First(&product, "name = ?", "Mouse")
+	fmt.Println(product)
+
+	// select all
+	var products []Product
+	db.Find(&products)
+	for _, product := range products {
+		fmt.Println(product)
 	}
-	db.Create(&products)
-	
 }
