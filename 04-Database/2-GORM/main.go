401c0fc80c78d130a54626b137572fe1991b36b0
torbenish <ishiitorben@gmail.com>
2024-06-16 17:31:35 -0300

    Add GORM/Soft Delete

--- 04-Database/2-GORM/main.go
+++ 04-Database/2-GORM/main.go
@@ -11,17 +11,18 @@ type Product struct {
 	ID    int `gorm:"primaryKey"`
 	Name  string
 	Price float64
+	gorm.Model
 }
 
 func main() {
-	dsn := "root:root@tcp(localhost:3306)/goexpert"
+	dsn := "root:root@tcp(localhost:3306)/goexpert?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
 	db.AutoMigrate(&Product{})
 
-	// create
+	//create
 	// db.Create(&Product{
 	// 	Name:  "Laptop",
 	// 	Price: 1000.00,
@@ -62,11 +63,13 @@ func main() {
 	// 	fmt.Println(product)
 	// }
 
-	var p Product
-	db.First(&p, 1)
-	p.Name = "Nem Mouse"
-	db.Save(&p)
+	// update
+	// var p Product
+	// db.First(&p, 1)
+	// p.Name = "Nem Mouse"
+	// db.Save(&p)
 
+	// delete
 	var p2 Product
 	db.First(&p2, 1)
 	fmt.Println(p2.Name)
