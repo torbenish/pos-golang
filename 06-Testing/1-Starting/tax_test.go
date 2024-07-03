c286e28a878544a1a6930be13f7f8e2793dc89e7
torbenish <ishiitorben@gmail.com>
2024-07-02 23:20:23 -0300

    Add Testing/Batch

--- 06-Testing/1-Starting/tax_test.go
+++ 06-Testing/1-Starting/tax_test.go
@@ -12,3 +12,22 @@ func TestCalculateTax(t *testing.T) {
 		t.Errorf("Expected %f, got %f", expected, result)
 	}
 }
+
+func TestCalculateTaxBatch(t *testing.T) {
+	type calcTax struct {
+		amount, espect float64
+	}
+
+	table := []calcTax{
+		{500.0, 5.0},
+		{1000.0, 10.0},
+		{1500.0, 12.0},
+	}
+
+	for _, item := range table {
+		result := CalculateTax(item.amount)
+		if result != item.espect {
+			t.Errorf("Expected %f, got %f", item.espect, result)
+		}
+	}
+}
