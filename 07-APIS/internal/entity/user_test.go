82f031c52dbb2d68979b6959d2af56d697cca0fe
torbenish <ishiitorben@gmail.com>
2024-07-14 15:34:56 -0300

    Teste API/Entity/User

---
+++ 07-APIS/internal/entity/user_test.go
@@ -0,0 +1,24 @@
+package entity
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewUser(t *testing.T) {
+	user, err := NewUser("John Doe", "jdoe@me.com", "123456")
+	assert.Nil(t, err)
+	assert.NotNil(t, user)
+	assert.NotEmpty(t, user.ID)
+	assert.NotEmpty(t, user.Password)
+	assert.Equal(t, "John Doe", user.Name)
+	assert.Equal(t, "jdoe@me.com", user.Email)
+}
+
+func TestValidatePassword(t *testing.T) {
+	user, err := NewUser("John Doe", "jdoe@me.com", "123456")
+	assert.Nil(t, err)
+	assert.True(t, user.ValidatePassword("123456"))
+	assert.False(t, user.ValidatePassword("1234567"))
+	assert.NotEqual(t, "123456", user.Password)
+}
