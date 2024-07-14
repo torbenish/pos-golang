2d00b7ed79d50516fe5b7b98331b5988e7c24382
torbenish <ishiitorben@gmail.com>
2024-07-14 16:44:27 -0300

    Test API/Database/UserDB/FindByEmail

--- 07-APIS/internal/infra/database/user_db_test.go
+++ 07-APIS/internal/infra/database/user_db_test.go
@@ -29,3 +29,23 @@ func TestCreateUser(t *testing.T) {
 	assert.Equal(t, user.Email, userFound.Email)
 	assert.NotNil(t, user.Password)
 }
+
+func TestFindByEmail(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(&entity.User{})
+	user, _ := entity.NewUser("John Doe", "jdoe@me.com", "123456")
+	userDB := NewUser(db)
+
+	err = userDB.Create(user)
+	assert.Nil(t, err)
+
+	userFound, err := userDB.FindByEmail(user.Email)
+	assert.Nil(t, err)
+	assert.Equal(t, user.ID, userFound.ID)
+	assert.Equal(t, user.Name, userFound.Name)
+	assert.Equal(t, user.Email, userFound.Email)
+	assert.NotNil(t, user.Password)
+}
