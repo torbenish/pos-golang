4961642aad9aace9c935e2378dc966256eecf2b2
torbenish <ishiitorben@gmail.com>
2024-07-21 17:17:59 -0300

    Create API/Routers

--- 07-APIS/cmd/server/main.go
+++ 07-APIS/cmd/server/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
 	"github.com/torbenish/pos-golang/07-APIS/configs"
 	"github.com/torbenish/pos-golang/07-APIS/internal/entity"
 	"github.com/torbenish/pos-golang/07-APIS/internal/infra/database"
@@ -24,6 +26,9 @@ func main() {
 	productDB := database.NewProduct(db)
 	productHandler := handlers.NewProductHandler(productDB)
 
-	http.HandleFunc("/products", productHandler.CreateProduct)
-	http.ListenAndServe(":8000", nil)
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Post("/products", productHandler.CreateProduct)
+
+	http.ListenAndServe(":8000", r)
 }
--- 07-APIS/go.mod
+++ 07-APIS/go.mod
@@ -3,6 +3,8 @@ module github.com/torbenish/pos-golang/07-APIS
 go 1.22.5
 
 require (
+	github.com/go-chi/chi v1.5.1
+	github.com/go-chi/chi/v5 v5.1.0
 	github.com/go-chi/jwtauth v1.2.0
 	github.com/google/uuid v1.4.0
 	github.com/spf13/viper v1.19.0
