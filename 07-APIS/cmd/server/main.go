1d91c5af6c7ab0f6ed366324a13f56241fa571d6
torbenish <ishiitorben@gmail.com>
2024-07-22 22:43:41 -0300

    Create delete and products_all functions in API/database

--- 07-APIS/cmd/server/main.go
+++ 07-APIS/cmd/server/main.go
@@ -30,7 +30,9 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Post("/products", productHandler.CreateProduct)
 	r.Get("/products/{id}", productHandler.GetProduct)
+	r.Get("/products", productHandler.GetProducts)
 	r.Put("/products/{id}", productHandler.UpdateProduct)
+	r.Delete("/products/{id}", productHandler.DeleteProduct)
 
 	http.ListenAndServe(":8000", r)
 }
