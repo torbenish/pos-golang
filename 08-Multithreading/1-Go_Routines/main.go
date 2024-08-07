4ca28f1de1a8022cea1bd5871fc24ce19e5d98ec
torbenish <ishiitorben@gmail.com>
2024-08-06 22:47:26 -0300

    Add MultiThreading/Go-Routines

---
+++ 08-Multithreading/1-Go_Routines/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func task(name string) {
+	for i := 0; i < 10; i++ {
+		fmt.Printf("%d: Task %s is running\n", i, name)
+		time.Sleep(1 * time.Second)
+	}
+}
+
+// Thread 1
+func main() {
+	// Thread 2
+	go task("A")
+	// Thead 3
+	go task("B")
+	// Nothing
+	// Exit
+	time.Sleep(15 * time.Second)
+}
