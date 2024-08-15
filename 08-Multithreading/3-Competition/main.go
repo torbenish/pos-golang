a7d008de623a4a6babc774e71a29b03a67c0d73c
torbenish <ishiitorben@gmail.com>
2024-08-15 15:54:18 -0300

    Corrections in Multithreading/Competition

--- 08-Multithreading/3-Competition/main.go
+++ 08-Multithreading/3-Competition/main.go
@@ -3,14 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 	"time"
 )
 
 var number uint64 = 0
 
 func main() {
+	// m := sync.Mutex{}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		number++
+		// m.Lock()
+		// number++
+		atomic.AddUint64(&number, 1)
+		// m.Unlock()
 		time.Sleep(300 * time.Millisecond)
 		w.Write([]byte(fmt.Sprintf("You are the visitor number %d", number)))
 	})
