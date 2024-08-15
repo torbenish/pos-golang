06c4dea4069db1ac05cabc34d37ad094d492fd35
torbenish <ishiitorben@gmail.com>
2024-08-15 15:19:41 -0300

    Add Multithreading/Competition

---
+++ 08-Multithreading/3-Competition/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var number uint64 = 0
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		number++
+		time.Sleep(300 * time.Millisecond)
+		w.Write([]byte(fmt.Sprintf("You are the visitor number %d", number)))
+	})
+	http.ListenAndServe(":3000", nil)
+}
